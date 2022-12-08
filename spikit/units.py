1cdd564ee504bf4c1c17105661329a0cade9939f
KittenPirate <friesbi7porto@gmail.com>
2022-12-08 17:11:25 +0200

    Add physical units and conversions

---
+++ spikit/units.py
@@ -0,0 +1,10 @@
+# Physical Constants
+GeVc2 = 1.78266192e-27 # kg
+G = 6.67430e-11 # m3/kg/s2, The Newtonian gravitational constant [Legacy 2012: 6.67408]
+c = 299792458 # m/s, The Speed of light in a vacuum
+
+# Useful Unit Conversions
+pc = 3.0856775814913673e16 # m, Parsec to metres
+Mo = 1.98847e30 # kg, Solar Mass to kg
+yr = 31556926 # s, A year in s
+day = 60*60*24 # s, A day in s
