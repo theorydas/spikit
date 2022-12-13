83fd201f53fcefce6778a65007023f251dd4c922
KittenPirate <friesbi7porto@gmail.com>
2022-12-13 14:54:46 +0200

    Add pi and remove energies

--- spikit/units.py
+++ spikit/units.py
@@ -1,5 +1,6 @@
+from numpy import pi
+
 # Physical Constants
-GeVc2 = 1.78266192e-27 # kg
 G = 6.67430e-11 # m3/kg/s2, The Newtonian gravitational constant [Legacy 2012: 6.67408]
 c = 299792458 # m/s, The Speed of light in a vacuum
 
