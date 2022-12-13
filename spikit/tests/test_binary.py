d3d42d273eb2f88b55eb07a872201c96815010e5
KittenPirate <friesbi7porto@gmail.com>
2022-12-13 20:41:49 +0200

    Introduce testing

--- spikit/tests/test_binary.py
+++ spikit/tests/test_binary.py
@@ -1,13 +1,44 @@
-from spikit.binary import *
+from spikit.binary import black_hole, binary
+from spikit.units import pi
 
-def test_total_mass():
+# ============================
+# ======== Mass Tests ========
+
+def test_calculate_total_mass():
     m1 = black_hole(10); m2 = black_hole(5)
-    b = binary(m1, m2, 100)
+    system = binary(m1, m2, 100)
     
-    assert b.m == 15 # [Msun]
+    assert system.m == 15 # [Msun]
 
-def test_mass_ratio():
+def test_calculate_mass_ratio():
     m1 = black_hole(1e4); m2 = black_hole(2)
-    b = binary(m1, m2, 100)
+    system = binary(m1, m2, 100)
     
-    assert b.mu == 5000
+    assert system.q == 0.0002
+
+# =============================
+# ======== Orbit Tests ========
+
+def test_fetch_orbit_elements():
+    a = 100; e = 0.45
+    
+    m1 = black_hole(1e4); m2 = black_hole(1)
+    system = binary(m1, m2, a, e)
+    
+    
+    assert system.a == a
+    assert system.e == e
+    
+def test_calculate_semiliatus_rectum():
+    m1 = black_hole(1e4); m2 = black_hole(2)
+    system = binary(m1, m2, 1e4, 0.5)
+    
+    assert system.p(system.a, system.e) == 7500
+
+def test_calculate_separation():
+    m1 = black_hole(1e4); m2 = black_hole(1)
+    system = binary(m1, m2, 100, 0.45)
+    
+    
+    assert system.r2(system.a, system.e, 0) == 55
+    assert system.r2(system.a, system.e, pi) == 145
---
+++ spikit/tests/test_evolution.py
@@ -0,0 +1,12 @@
+from spikit.binary import black_hole, binary
+from spikit.units import pi
+
+# ============================
+# ======== Mass Tests ========
+
+def test_zero_evolution():
+    m1 = black_hole(10); m2 = black_hole(5)
+    system = binary(m1, m2, 100)
+
+    assert system.da_dt(0, 0, 50, 100) == 0 # [pc/s]
+    assert system.de_dt(0, 0, 0, 50, 100, 0.45)  == 0 # [1/s]
