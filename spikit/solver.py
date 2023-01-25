baa6bcfa742a619870c696719edb46f7810a5b54
KittenPirate <friesbi7porto@gmail.com>
2023-01-25 14:03:49 +0100

    Initialize solver.py

---
+++ spikit/solver.py
@@ -0,0 +1,13 @@
+# A class that takes care of the integration.
+
+class StaticSolver():
+    """ The solver class for the static problem. """
+    
+    def __init__(self) -> None:
+        pass
+    
+    def update(a: float, t: float, h: float):
+        return
+    
+    def solve():
+        return
