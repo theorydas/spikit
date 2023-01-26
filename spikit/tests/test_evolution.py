97fc9c37f817ba59afdfc16b1cf8b035450879e9
KittenPirate <friesbi7porto@gmail.com>
2023-01-26 17:48:03 +0100

    Add analytical solutions to df-only systems

--- spikit/tests/test_evolution.py
+++ spikit/tests/test_evolution.py
@@ -1,11 +1,11 @@
 from spikit.binary import Binary
 from spikit.forces import GravitationalWaves, DynamicalFrictionIso, AccretionIso
-from spikit.units import yr
+from spikit.units import yr, day, pc
 from spikit.solvers import StaticSolver
 from spikit.spike import StaticPowerLaw
 
 
-from spikit.tests.fixtures import default_binary
+from spikit.tests.fixtures import default_binary, default_spike
 from pytest import approx
 from numpy import sum
 
@@ -51,3 +51,45 @@ def test_zero_density_evolution(default_binary: Binary):
     t_spike, a_spike = StaticSolver(binary, [gw, df, acc]).solve(a0, h = 1e-1, order = 1)
     
     assert sum(t_vacuum -t_spike) == approx(0, rel = 1e-1)
+
+def test_reconstruct_published_merger():
+    """ According to the paper arXiv:2002.12811v2, VB, page 14,
+    this merger should be sped up by 48 days in 5 years. This used the old Coulomb logarithm.
+    """
+    binary = Binary(1.4e4, 1.4)
+    spike = StaticPowerLaw(binary, 7/3, rhosp = 226)
+
+    gw = GravitationalWaves(binary)
+    df = DynamicalFrictionIso(spike)
+    df.b_eff = lambda r2, u, q: r2 *pc *(q)**(0.5) # For old Coulomb logarithm.
+    
+    t, _ = StaticSolver(binary, [gw, df]).solve(gw.vacuum_merger_distance(5 *yr), h = 1e-2)
+    
+    assert (5 *yr -t[-1])/day == approx(48, rel = 1e-1)
+
+def test_df_merger_time(default_spike: StaticPowerLaw):    
+    spike = default_spike
+    
+    gw = GravitationalWaves(spike.binary)
+    df = DynamicalFrictionIso(spike)
+    
+    t_vacuum_merger = 5 *yr
+    a0 = gw.vacuum_merger_distance(t_vacuum_merger)
+    t_df_merger = df.df_merger_time(a0)
+    
+    t, a = StaticSolver(spike.binary, [gw, df]).solve(a0, h = 1e-2, order = 1)
+    
+    assert t[-1] == approx(t_df_merger, rel = 1e-1)
+
+def test_zero_density_merger_time(default_binary: Binary):    
+    spike = StaticPowerLaw(default_binary, 7/3, rhosp = 0)
+    
+    gw = GravitationalWaves(spike.binary)
+    df = DynamicalFrictionIso(spike)
+    
+    a0 = 100 *spike.binary.Risco() # [pc]
+    
+    t_df_merger = df.df_merger_time(a0)
+    t_df_merger = gw.vacuum_merger_time(a0)
+    
+    assert t_df_merger == t_df_merger
