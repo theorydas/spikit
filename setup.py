e2f75c3464ce56f3e48de714546043d08f5fe78d
KittenPirate <friesbi7porto@gmail.com>
2023-02-03 17:05:58 +0100

    Include description & url +requirements

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ import setuptools
 setuptools.setup(
     name = "spikit",
     version = "1.0",
-    author = "KitenPirate"
+    author = "KitenPirate",
+    description="A package for the semi-analytical modelling of dressed binaries.",
+    url="https://github.com/theorydas/spikit",
+    install_requires = ["numpy", "scipy"],
 )
